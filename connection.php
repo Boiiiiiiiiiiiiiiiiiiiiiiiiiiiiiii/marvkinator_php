f7e5f5b654c7b77204d8ef421424c5c7c711d492
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2025-04-28 10:08:13 +0500

    Обновил connection.php под clever cloud

--- connection.php
+++ connection.php
@@ -4,10 +4,11 @@
   header('Content-Type: application/json');
   header("Access-Control-Allow-Origin: *");
 
-  $servername = "sql302.infinityfree.com";
-  $username = "if0_38847752";
-  $password = "v97bmsgREYD2";
-  $dbname = "if0_38847752_akinator_db";
+  $servername = "bncjpie37hb9qataurjt-mysql.services.clever-cloud.com";
+  $username = "uyn3ieoabrdydg0k";
+  $password = "XyoRz4khyfh27MMn5TeE";
+  $dbname = "bncjpie37hb9qataurjt";
+  $port = "your-new-port";
 
   try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
